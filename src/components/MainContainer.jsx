a262befe1c7f1c168dcd4ce6639e421e6019b9e5
Leyla Aziz <ezizovaleyla1@gmail.com>
2022-09-09 13:00:12 +0400

    filter items

--- src/components/MainContainer.jsx
+++ src/components/MainContainer.jsx
@@ -4,12 +4,16 @@ import { motion } from "framer-motion";
 import { MdChevronLeft, MdChevronRight } from "react-icons/md";
 import { useStateValue } from "../context/StateProvider";
 import RowContainer from "./RowContainer";
+import MenuContainer from "./MenuContainer";
+import CartContainer from "./CartContainer";
 
 const MainContainer = () => {
   const [scrollValue, setScrollValue] = useState(0);
 
   const [{ foodItems }, dispatch] = useStateValue();
-  console.log(foodItems);
+
+  useEffect(() => {}, [scrollValue]);
+
   return (
     <div className="w-full h-auto flex flex-col items-center justify-center ">
       <HomeContainer />
@@ -38,10 +42,13 @@ const MainContainer = () => {
           </div>
         </div>
         <RowContainer
+          scrollValue={scrollValue}
           flag={true}
           data={foodItems?.filter((item) => item.category === "fruits")}
         />
       </section>
+      <MenuContainer />
+      <CartContainer />
     </div>
   );
 };
